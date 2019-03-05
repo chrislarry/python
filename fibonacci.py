e020fceeefe4982bb1f1537e916cccdab8754564
chrislarry <47925529+chrislarry@users.noreply.github.com>
2019-03-05 02:56:17 +0200

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -4,14 +4,6 @@ count = 10
 old = 0
 
 print("\n\nFibonacci number\n\n")
-#count = input("dose no numero: ")
-"""
-for w in range(count):
-    res = new + old
-    old = new
-    new = res
-    print(new)
-"""
 
 for i in range(count):
     new += old
